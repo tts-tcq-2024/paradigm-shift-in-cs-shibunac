64369e74e2d7eb5a0d1898bd4ef26077288bd965
shibunac <shibunachedakal@gmail.com>
2024-09-06 14:27:31 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -4,20 +4,38 @@ namespace paradigm_shift_csharp
 {
 class Checker
 {
-    static bool batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
-            Console.WriteLine("Temperature is out of range!");
-            return false;
-        } else if(soc < 20 || soc > 80) {
-            Console.WriteLine("State of Charge is out of range!");
-            return false;
-        } else if(chargeRate > 0.8) {
-            Console.WriteLine("Charge Rate is out of range!");
-            return false;
-        }
-        return true;
+    const float TEMP_MIN = 0;
+    const float TEMP_MAX = 45;
+    const float SOC_MIN = 20;
+    const float SOC_MAX = 80;
+    const float CHARGERATE_MIN = 0.8;
+    const string INVALID_RANGE = "Out of Range";
+    
+
+    static bool isTemperatureOk(float temperature){
+        return (temperature < 0 || temperature > 45) ? false : true;
     }
 
+    static bool isSoCOk(float soc){
+        return (soc < 20 || soc > 80) ? false : true;
+    }
+
+    static bool isChargeRateOk(float chargeRate){
+        return (chargeRate > 0.8) ? false : true
+    }
+        
+    static bool isBatteryOk(float temperature, float soc, float chargeRate){
+        bool tempCheck = isTemperatureOk(temperature);
+        bool socCheck = isSoCOk(soc);
+        bool chargeRateCheck = isChargeRateOk(chargeRate);
+
+        return tempCheck && socCheck && chargeRateCheck;
+    }   
+
+    static string DisplayMessage(){
+        
+    }
+    
     static void ExpectTrue(bool expression) {
         if(!expression) {
             Console.WriteLine("Expected true, but got false");
